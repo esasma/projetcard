68ad8ce5fc5308a6898aaaf302a43004a7d0816c
esasma <95236682+esasma@users.noreply.github.com>
2022-09-09 10:40:22 +0200

    Add files via upload

---
+++ src/pages/liste.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    import card from "../components/card.vue" ;
+
+    const maisons = [ {nom:"test", prix:24}, { nom:"test2", prix:24 },{ nom:"test3", prix:"24" },{ nom:"test5", prix:"24" }];
+    
+</script>
+<template>
+    <div class="p-2">
+        <h1 class="text-2xl">Page liste </h1>
+        <div v-for="maison in maisons" :key="maison.nom" >
+            <card v-bind="maison"/>
+        </div>
+    </div>
+</template>
