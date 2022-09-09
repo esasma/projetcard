1712a9d81923b757248640b15dc5bf9a83761766
esasma <95236682+esasma@users.noreply.github.com>
2022-09-09 10:39:47 +0200

    Add files via upload

---
+++ src/pages/simple.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+   import card from "../components/card.vue" ;
+   import maisons from "../assets/maisons.json";
+
+   const obj={
+nom:"test1",
+prix:"34",
+   };
+
+</script>
+<template>
+    <card nom="nom de lamaison" :prix="34"/>
+    <card v-bind="obj" />
+</template>
